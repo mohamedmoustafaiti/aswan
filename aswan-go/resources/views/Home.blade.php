65063b54ea941699337518b0aabe6141689ae029
mohamedmoustafaiti <mohamedmoustafaiti@gmail.com>
2020-03-25 11:54:14 +0200

    create home page and change route

---
+++ aswan-go/resources/views/Home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <style>
+    header , footer{
+        background-color:black;
+        height:100px;
+    }
+    .container{
+        height:600px;
+        width:80%;
+        margin:auto;
+    }
+    </style>
+</head>
+<body>
+<header></header>
+<div class="container">
+    <h3>Welcome to home page</h3>
+</div>
+<footer></footer>
+    
+</body>
+</html>
